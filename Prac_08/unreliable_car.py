c7d501f2e07ddbe139a85266934c196432c006bc
Jascha <jascha.coetser@my.jcu.edu.au>
2019-05-16 12:10:12 +1000

    Created unreliable_car

---
+++ Prac_08/unreliable_car.py
@@ -0,0 +1,23 @@
+"""
+Unreliable Car, derived from Car
+"""
+
+from random import randint
+from Prac_08.car import Car
+
+
+class UnreliableCar(Car):
+    """ An unreliable version of a car"""
+
+    def __int__(self, name, fuel, reliability):
+        """Initialise an UnreliableCar"""
+        super().__init__(name, fuel)
+        self.reliability = reliability
+
+    def drive(self, distance):
+        """Drive the car"""
+        random_number = randint(1, 100)
+        if random_number >= self.reliability:
+            distance = 0
+        distance_driven = super().drive(distance)
+        return distance_driven
