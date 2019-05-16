13ff92954de59ebee83427463d49a7cc64803f5e
Jascha <jascha.coetser@my.jcu.edu.au>
2019-05-16 14:14:20 +1000

    finished Prac_08

--- Prac_08/taxi_simulator.py
+++ Prac_08/taxi_simulator.py
@@ -2,7 +2,6 @@
 Taxi Simulator
 """
 
-from Prac_08.car import Car
 from Prac_08.taxi import Taxi
 from Prac_08.silver_service_taxi import SilverServiceTaxi
 
@@ -45,40 +44,9 @@ def main():
 
 
 def display_taxis(taxis):
-    """Display numbered list of taxis."""
+    """Numbered list of taxis"""
     for i, taxi in enumerate(taxis):
         print("{} - {}".format(i, str(taxi)))
-
-
-def run_tests():
-    """Run tests to show workings of Car and Taxi classes."""
-    bus = Car("Datsun", 180)
-    bus.drive(30)
-    print("fuel =", bus.fuel)
-    print("odo =", bus.odometer)
-    bus.drive(55)
-    print("fuel =", bus.fuel)
-    print("odo = ", bus.odometer)
-    print(bus)
-
-    distance = int(input("Drive how far? "))
-    while distance > 0:
-        travelled = bus.drive(distance)
-        print("{} travelled {}".format(str(bus), travelled))
-        distance = int(input("Drive how far? "))
-
-    t = Taxi("Prius 1", 100)
-    print(t)
-    t.drive(25)
-    print(t, t.get_fare())
-    t.start_fare()
-    t.drive(40)
-    print(t, t.get_fare())
-
-    sst = SilverServiceTaxi("Limo", 100, 2)
-    print(sst, sst.get_fare())
-    sst.drive(10)
-    print(sst, sst.get_fare())
 
 
 main()
